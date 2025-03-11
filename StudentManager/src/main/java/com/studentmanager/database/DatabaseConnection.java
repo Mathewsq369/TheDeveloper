5d5b0bdab6e337daa2b272de4e4259494aa245f2
WacoderForever <sethomondi088@gmail.com>
2025-03-11 16:14:33 +0300

    dwhjewds

--- StudentManager/src/main/java/com/studentmanager/database/DatabaseConnection.java
+++ StudentManager/src/main/java/com/studentmanager/database/DatabaseConnection.java
@@ -4,13 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DatabaseConnection {
-    private static final String JDBC_URL="jdbc:mysql://localhost:3306/student_db";
+    private static final String JDBC_URL="jdbc:mariadb://localhost:3306/student_manager";
     private static final String JDBC_USER="root";
-    private static final String JDBC_PASSWORD="Shamha@2002";
+    private static final String JDBC_PASSWORD="seth2019";
     
     public static Connection getConnection(){
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
+            Class.forName("org.mariadb.jdbc.Driver");
             return DriverManager.getConnection(JDBC_URL,JDBC_USER,JDBC_PASSWORD);
         }catch(Exception e){
             e.printStackTrace();
